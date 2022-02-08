e13e319da6c821fcfbcb91b87ae6556171506b8e
tobbyluvsky <tobby995@gmail.com>
2022-02-08 16:16:49 +0100

    admin database and seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Admin::insert([
+            'name' => 'tobbyluvsky',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('oluwadamilola1'),
+            'phone' => '09059153987',
+            'address' => 'lagos state ojo',
+            'role_id' => 1,
+            'status' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
             </div>
             <!-- /Page Header -->
 
-          
+
 
         </div>
         <!-- /Page Content -->
